f3972bce986be03d0b778d4a204c0f9aaa29093d
Shivam gupta <sguupta1214@gmail.com>
2024-08-31 04:53:29 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use((err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    stack: process.env.NODE_ENV === 'productio' ? null : err.stack,
   });
 });
 
