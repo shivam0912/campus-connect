cc479fb056dd2729183407295dfddb64feb1f482
Shivam gupta <sguupta1214@gmail.com>
2024-08-31 04:00:05 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,9 @@ app.get('/api/config/cloudinarypreset', (req, res) => {
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+  // Remove the frontend serving code here
 } else {
   app.get('/', (req, res) => {
     res.send('API is running...')
