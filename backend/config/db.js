7b89af7ea68d307af087283a85c5b5eb0d80a204
Shivam gupta <sguupta1214@gmail.com>
2024-08-31 05:15:29 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
-import 'dotenv/config';
-
+import 'dotenv/config'
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
+      serverSelectionTimeoutMS: 5000, // Timeout for server selection
+      socketTimeoutMS: 45000, // Timeout for sockets
+      retryWrites: true // Enable retryable writes
     })
     console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (err) {
-    console.error(err)
+    console.error('Error connecting to MongoDB:', err)
     process.exit(1)
   }
 }
